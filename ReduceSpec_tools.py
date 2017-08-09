a979638c98454d81cdcfb852b27c80f7e1fa97d6
Ben Kaiser <bkaiser94@gmail.com>
2017-08-09 17:49:52 -0400

    reversion to slow double for loop for median combining images (without numpy 1.9 I don't really see a way around it.) It is curious though that the code seemed to execute just fine with the previous format as in the last few runs. I successfully produced images. I'm not sure what's going on here...

--- ReduceSpec_tools.py
+++ ReduceSpec_tools.py
@@ -1352,18 +1352,21 @@ def imcombine(im_list, output_name, method,
         if method == 'median':
             counts = np.copy(img_block)
             clipped_sig= SigClip(counts, lo_sig, hi_sig)
-            print "clipped_sig: ", clipped_sig
-            print "clipped_sig.shape: ", clipped_sig.shape
-            print "clipped_sig[~np.isnan(clipped_sig)].shape: ", clipped_sig[~np.isnan(clipped_sig)].shape
-            val = np.median( clipped_sig[~np.isnan(clipped_sig)], axis=0 )
-            print "val: ", val
-            print "val.shape : ", val.shape
-            print "np.median(clipped_sig, axis =0): ", np.median(clipped_sig, axis=0)
-            comb_img[0,:,:] = np.copy(val)
-            #for y in range(0,Ny):
-                #for x in range(0,Nx):
-                    #mini_clip= clipped_sig[:, y, x]
-                    #val = np.median( mini_clip[~np.isnan(mini_clip)], axis=0 )
+            #print "clipped_sig: ", clipped_sig
+            #print "clipped_sig.shape: ", clipped_sig.shape
+            #print "clipped_sig[~np.isnan(clipped_sig)].shape: ", clipped_sig[~np.isnan(clipped_sig)].shape
+            #val = np.median( clipped_sig[~np.isnan(clipped_sig)], axis=0 )
+            #val = np.nanmedian(clipped_sig, axis = 0) #solution when numpy is upgraded to 1.9 or later. Only line required other than then inserting this into the combined array
+            #print "val: ", val
+            #print "val.shape : ", val.shape
+            print "If this numpy version is 1.9 or later, we need to update the code since 1.8.2 doesn't have np.nanmedian... : ", np.__version___
+            #print "np.median(clipped_sig, axis =0): ", np.median(clipped_sig, axis=0)
+            #comb_img[0,:,:] = np.copy(val)
+            for y in range(0,Ny):
+                for x in range(0,Nx):
+                    mini_clip= clipped_sig[:, y, x]
+                    val = np.median( mini_clip[~np.isnan(mini_clip)], axis=0 )
+                    comb_img[0,y,x] = np.float32(val)
                     #counts = img_block[:,y,x]
                     #val = np.median( SigClip(counts, lo_sig, hi_sig) )
                     #comb_img[0,y,x] = np.float32(val)
